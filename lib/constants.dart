d8e73099f2c4693d3b1641a5656b0c437d5ee51a
Nickolay Romensky <nromensky@gmail.com>
2021-03-29 18:42:17 +0300

    .

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const myButtonTextStyle = TextStyle(fontSize: 37, fontFamily: 'XanhMono');
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:chat_flutter_app/constants.dart';
 
 class WelcomeScreen extends StatelessWidget {
   static const id = 'Welcome Screen';
@@ -10,11 +12,35 @@ class WelcomeScreen extends StatelessWidget {
         title: Text('Chat'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
+            flex: 2,
             child: Icon(Icons.bolt, size: 200, color: Colors.amberAccent),
           ),
-          Expanded(child: CupertinoButton())
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 32),
+              child: CupertinoButton.filled(
+                onPressed: () {},
+                child: Text('Register', style: myButtonTextStyle),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 32),
+              child: CupertinoButton.filled(
+                onPressed: () {},
+                child: Text('Login', style: myButtonTextStyle),
+              ),
+            ),
+          ),
+          Expanded(
+              child: SizedBox(
+            height: 32,
+          ))
         ],
       ),
     );
