2e7aa447941bbc81977f79bc9e8506898a8e8fa0
Nickolay Romensky <nromensky@gmail.com>
2021-04-04 04:59:39 +0300

    .

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
 
 import 'package:chat_flutter_app/resources.dart';
 
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:email_validator/email_validator.dart';
 
